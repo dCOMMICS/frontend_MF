81940eaea2001ed929a8c23d0793e17c785cf913
dCOMMICS <sanwurld@gmail.com>
2024-07-31 00:12:18 +0300

    initial commit

--- chess2/index.js
+++ chess2/index.js
@@ -1,7 +1,28 @@
-function insertImages() {
-    document.querySelectorAll('.box').forEach (image => {
-        if(image.innerText.length !==0){
-            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn')
+// function insertImage() {
+//     document.querySelectorAll('.box').forEach (image => {
+//         if(image.innerText.length !==0){
+//             if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+//                 image.innerHTML = `${image.innerText} <img class = 'all-img all-pown' src="${image.innerHTML}.png" alt =""`> 
+//                 image.style.cursor = 'pointer'
+//             }
+//         }
+//     })
+// }
+
+// insertImage()
+
+function insertImage() {
+    document.querySelectorAll('.box').forEach(image => {
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = `${image.innerText} <img class='all-img all-pown' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+            else {
+                image.innerHTML = `${image.innerText} <img class='all-img' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
         }
     })
 }
+insertImage()
